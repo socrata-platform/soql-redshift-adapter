c2517b71f9e503e60d2efc48eb6d8e0ab7beb8eb
PaulMTT <109166392+PaulMTT@users.noreply.github.com>
2023-09-27 10:44:46 -0400

    EN-62054: openapi server gen mvp

---
+++ src/main/scala/com/socrata/controller/VersionController.scala
@@ -0,0 +1,10 @@
+package com.socrata.controller
+
+import com.socrata.api.VersionApi
+import com.socrata.model.VersionInfo
+import jakarta.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class VersionController extends VersionApi{
+  override def versionGet(): VersionInfo = ???
+}
