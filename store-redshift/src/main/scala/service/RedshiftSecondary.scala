3358fa3c67c35b4b8c5085acc56dfce04b0f0968
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-10 11:08:43 -0400

    placeholder store datasource, added comments

--- store-redshift/src/main/scala/service/RedshiftSecondary.scala
+++ store-redshift/src/main/scala/service/RedshiftSecondary.scala
@@ -1,12 +1,12 @@
 package service
 
+import _root_.config.RedshiftSecondaryConfig
 import com.rojoma.simplearm.v2.Managed
 import com.socrata.datacoordinator.secondary.Secondary.Cookie
 import com.socrata.datacoordinator.secondary._
 import com.socrata.datacoordinator.truth.metadata.IndexDirective
 import com.socrata.datacoordinator.util.collection.ColumnIdMap
 import com.socrata.soql.types.{SoQLType, SoQLValue}
-import _root_.config.RedshiftSecondaryConfig
 import io.agroal.api.AgroalDataSource
 import io.quarkus.agroal.DataSource
 import jakarta.enterprise.context.ApplicationScoped
@@ -15,7 +15,11 @@ import meta.service.MetaService
 
 @ApplicationScoped
 class RedshiftSecondary(
-                         metaService:MetaService,
+                         //This represents the meta datasource, our metadata is easy to fit into orm
+                         metaService: MetaService,
+                         //Possibly no orm? Unclear so far, lets just use a datasource for now.
+                         @DataSource("store")
+                         storeDatasource: AgroalDataSource,
                          config: RedshiftSecondaryConfig
                        ) extends Secondary[SoQLType, SoQLValue] {
   override def shutdown(): Unit = ???
