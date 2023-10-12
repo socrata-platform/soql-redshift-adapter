c284b80f959b1b179ca19178a2bec4b032272913
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-12 14:49:12 -0400

    100k records batched in 10k and 100k

--- store-redshift/src/test/scala/com/socrata/InsertTest.scala
+++ store-redshift/src/test/scala/com/socrata/InsertTest.scala
@@ -1,20 +1,21 @@
 package com.socrata
 
-import com.github.tototoshi.csv.{CSVReader, DefaultCSVFormat, QUOTE_ALL, QUOTE_NONNUMERIC, Quoting}
+import scala.collection.JavaConversions._
+import com.opencsv.{CSVParserBuilder, CSVReader, CSVReaderBuilder}
 import com.socrata.util.Timing
 import io.agroal.api.AgroalDataSource
 import io.quarkus.agroal.DataSource
 import io.quarkus.test.junit.QuarkusTest
 import jakarta.inject.Inject
-import org.junit.jupiter.api.{BeforeEach, DisplayName, Test}
+import org.junit.jupiter.api.{BeforeEach, DisplayName, Test, Timeout}
 
-import scala.io.Source
+import java.util.concurrent.TimeUnit
+import scala.io.{BufferedSource, Source}
 import scala.util.Using
 
 @DisplayName("Redshift insert tests")
 @QuarkusTest
 class InsertTest {
-
   @DataSource("store")
   @Inject
   var dataSource: AgroalDataSource = _
@@ -41,39 +42,82 @@ class InsertTest {
     }
   }
 
-  @DisplayName("via JDBC sequential")
+  def readTestData(path:String)={
+    val reader = Source.fromURL(getClass.getResource(path))
+
+    val parser = new CSVParserBuilder()
+      .withSeparator(',')
+      .withIgnoreQuotations(false)
+      .withIgnoreLeadingWhiteSpace(true)
+      .withStrictQuotes(false)
+      .build();
+
+    val csvReader = new CSVReaderBuilder(reader.reader())
+      .withSkipLines(1)
+      .withCSVParser(parser)
+      .build();
+    csvReader
+  }
+
+  @DisplayName("100k rows via 10k batch, JDBC")
   @Test
-  def jdbcSequential() = {
+  def jdbcSequential():Unit = {
+    val data = readTestData("/data/hdyn-4f6y/data.csv")
+    val batchSize = 10000
     Timing.Timed {
       Using.resource(dataSource.getConnection) { conn =>
-        for (elem <- CSVReader.open(Source.fromURL(getClass.getResource("/data/hdyn-4f6y/data.csv"))).iteratorWithHeaders) {
-          Using.resource(conn.prepareStatement("""insert into "hdyn-4f6y"(fiscal_year, department_name, supplier_name, description, procurement_eligible, cert_supplier, amount, cert_classification) values(?,?,?,?,?,?,?,?);""".stripMargin)){stmt=>
-            try{
-              stmt.setLong(1, elem("fiscal_year").toLong)
-              stmt.setString(2, elem("department_name"))
-              stmt.setString(3, elem("supplier_name"))
-              stmt.setString(4, elem("description"))
-              stmt.setString(5, elem("procurement_eligible"))
-              stmt.setString(6, elem("cert_supplier"))
-              stmt.setLong(7, elem("amount").toLong)
-              stmt.setString(8, elem("cert_classification"))
-            }catch {
-              case e=> println(s"Error: elem:'$elem', err:'$e'")
+        Using.resource(conn.prepareStatement("""insert into "hdyn-4f6y"(fiscal_year, department_name, supplier_name, description, procurement_eligible, cert_supplier, amount, cert_classification) values(?,?,?,?,?,?,?,?);""".stripMargin)) { stmt =>
+          var count = 0
+          for (elem <- data.iterator()) {
+            stmt.setLong(1, elem(0).trim.toLong)
+            stmt.setString(2, elem(1).trim)
+            stmt.setString(3, elem(2).trim)
+            stmt.setString(4, elem(3).trim)
+            stmt.setString(5, elem(4).trim)
+            stmt.setString(6, elem(5).trim)
+            stmt.setLong(7, elem(6).trim.toLong)
+            stmt.setString(8, elem(7).trim)
+            stmt.addBatch()
+            count+=1
+            if (count%batchSize==0){
+              stmt.executeUpdate()
             }
-            stmt.executeUpdate()
           }
+          stmt.executeUpdate()
         }
+
       }
-    }{elapsed=>
+    } { elapsed =>
       println(s"Inserting via JDBC sequential took $elapsed")
     }
   }
 
-  @DisplayName("via JDBC batch")
+  @Timeout(value = 10, unit = TimeUnit.SECONDS)
+  @DisplayName("100k rows all batched, JDBC")
   @Test
-  def jdbcBatch() = {
-    Using.resource(dataSource.getConnection) { conn =>
+  def jdbcBatch(): Unit = {
+    val data = readTestData("/data/hdyn-4f6y/data.csv")
 
+    Timing.Timed {
+      Using.resource(dataSource.getConnection) { conn =>
+        Using.resource(conn.prepareStatement("""insert into "hdyn-4f6y"(fiscal_year, department_name, supplier_name, description, procurement_eligible, cert_supplier, amount, cert_classification) values(?,?,?,?,?,?,?,?);""".stripMargin)) { stmt =>
+          for (elem <- data.iterator()) {
+            stmt.setLong(1, elem(0).trim.toLong)
+            stmt.setString(2, elem(1).trim)
+            stmt.setString(3, elem(2).trim)
+            stmt.setString(4, elem(3).trim)
+            stmt.setString(5, elem(4).trim)
+            stmt.setString(6, elem(5).trim)
+            stmt.setLong(7, elem(6).trim.toLong)
+            stmt.setString(8, elem(7).trim)
+            stmt.addBatch()
+          }
+          stmt.executeUpdate()
+        }
+
+      }
+    } { elapsed =>
+      println(s"Inserting via JDBC sequential took $elapsed")
     }
   }
 
