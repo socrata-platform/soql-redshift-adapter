4059c2db9e3d430be48b84babca87c8f40b86d71
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-12 10:11:46 -0400

    redshift connection sanity test

--- store-redshift/src/test/scala/com/socrata/SanityTest.scala
+++ store-redshift/src/test/scala/com/socrata/SanityTest.scala
@@ -1,13 +1,28 @@
 package com.socrata
 
+import io.agroal.api.AgroalDataSource
+import io.quarkus.agroal.DataSource
 import io.quarkus.test.junit.QuarkusTest
-import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+import org.junit.jupiter.api.{DisplayName, Test}
 
+import scala.util.Using
+
+@DisplayName("Sanity Tests")
 @QuarkusTest
 class SanityTest() {
+  @DataSource("store")
+  @Inject
+  var dataSource: AgroalDataSource = _
+
+  @DisplayName("Connect to redshift jdbc")
   @Test
-  def one() = {
-    println("Hello world!")
+  def connectToRedshiftJdbc() = {
+    Using(dataSource.getConnection) { conn =>
+      val databaseName = conn.getMetaData.getDatabaseProductName;
+      println(s"Database name: '$databaseName'")
+      assert(databaseName=="Redshift")
+    }.get
   }
 
 }
