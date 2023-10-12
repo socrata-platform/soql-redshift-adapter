631a64ea52710dacc656548a6ccc89831f4adb76
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-12 11:39:23 -0400

    create table setup for redshift insert tests

--- store-redshift/src/test/scala/com/socrata/InsertTest.scala
+++ store-redshift/src/test/scala/com/socrata/InsertTest.scala
@@ -4,32 +4,57 @@ import io.agroal.api.AgroalDataSource
 import io.quarkus.agroal.DataSource
 import io.quarkus.test.junit.QuarkusTest
 import jakarta.inject.Inject
-import org.junit.jupiter.api.{DisplayName, Test}
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.extension.{BeforeAllCallback, ExtensionContext}
+import org.junit.jupiter.api.{Assertions, BeforeAll, BeforeEach, DisplayName, Test}
 
+import java.sql.SQLException
 import scala.util.Using
 
 @DisplayName("Redshift insert tests")
 @QuarkusTest
-class InsertTest {
+class InsertTest{
 
   @DataSource("store")
   @Inject
   var dataSource: AgroalDataSource = _
 
+  @BeforeEach
+   def beforeEach(): Unit = {
+    Using.resource(dataSource.getConnection) { conn =>
+      Using.resource(conn.createStatement()){stmt=>
+          stmt.executeUpdate(
+            """
+              |create temporary table if not exists "hdyn-4f6y"(
+              |fiscal_year bigint not null,
+              |department_name text not null,
+              |supplier_name text not null,
+              |description text not null,
+              |procurement_eligible text not null,
+              |cert_supplier text not null,
+              |amount bigint not null,
+              |cert_classification text not null
+              |);
+              |truncate table "hdyn-4f6y";
+              |""".stripMargin)
+      }
+    }
+  }
+
   @DisplayName("via JDBC sequential")
   @Test
   def jdbcSequential() = {
-    Using(dataSource.getConnection){conn=>
+    Using.resource(dataSource.getConnection){conn=>
 
-    }.get
+    }
   }
 
   @DisplayName("via JDBC batch")
   @Test
   def jdbcBatch() = {
-    Using(dataSource.getConnection) { conn =>
+    Using.resource(dataSource.getConnection) { conn =>
 
-    }.get
+    }
   }
 
   @DisplayName("via S3")
