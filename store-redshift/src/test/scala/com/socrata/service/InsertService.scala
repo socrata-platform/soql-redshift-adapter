add8386603aa3129729193ab6af62116ffe32cd3
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-13 16:15:11 -0400

    ah right, query resultset should also be closed

--- store-redshift/src/test/scala/com/socrata/service/InsertService.scala
+++ store-redshift/src/test/scala/com/socrata/service/InsertService.scala
@@ -32,13 +32,14 @@ class InsertService
   def getTableRowCount(tableName:String):Option[Long]={
     Using.resource(dataSource.getConnection) { conn =>
       Using.resource(conn.prepareStatement(s"""select count(*) from "$tableName";""")) { stmt =>
-        val resultset = stmt.executeQuery();
-        val rowcount:Option[Long] = extract(resultset)(rs => rs.getLong(1)).headOption;
-        rowcount match{
-          case Some(count)=> println(s"Table $tableName has $count records")
-          case _=> println(s"Unable to get record count for table $tableName")
+        Using.resource(stmt.executeQuery()){resultSet=>
+          val rowcount: Option[Long] = extract(resultSet)(rs => rs.getLong(1)).headOption;
+          rowcount match {
+            case Some(count) => println(s"Table $tableName has $count records")
+            case _ => println(s"Unable to get record count for table $tableName")
+          }
+          rowcount
         }
-        rowcount
       }
     }
   }
