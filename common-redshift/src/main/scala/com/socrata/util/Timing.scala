2d841e966135d931363031af2514bf31bda8f31e
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-18 13:01:31 -0400

    checkpoint

--- common-redshift/src/main/scala/com/socrata/util/Timing.scala
+++ common-redshift/src/main/scala/com/socrata/util/Timing.scala
@@ -12,26 +12,4 @@ object Timing {
     result
   }
 
-  def TimedResult[T](block: => T)(fun: (T, Duration) => Unit): T = {
-    val start = System.currentTimeMillis()
-    val result = block
-    val end = System.currentTimeMillis()
-    fun(result, Duration(end - start, MILLISECONDS))
-    result
-  }
-
-  def TimedResultReturning[T,P](block: => T)(fun: (T, Duration) => P): P = {
-    val start = System.currentTimeMillis()
-    val result = block
-    val end = System.currentTimeMillis()
-    fun(result, Duration(end - start, MILLISECONDS))
-  }
-
-  def TimedResultReturningTransformed[T, P](block: => T)(fun: (T, Duration) => P): P = {
-    val start = System.currentTimeMillis()
-    val result = block
-    val end = System.currentTimeMillis()
-    fun(result, Duration(end - start, MILLISECONDS))
-  }
-
 }
--- store-redshift/src/main/scala/config/RedshiftSecondaryDependencies.scala
+++ store-redshift/src/main/scala/config/RedshiftSecondaryDependencies.scala
@@ -1,14 +1,21 @@
 package config
 
+import com.codahale.metrics.MetricRegistry
 import com.socrata.datacoordinator.common.DataSourceFromConfig.DSInfo
 import com.socrata.datacoordinator.secondary.Secondary
 import com.socrata.datacoordinator.secondary.SecondaryWatcherApp.NumWorkers
 import com.socrata.soql.types.{SoQLType, SoQLValue}
-import com.socrata.thirdparty.metrics.MetricsReporter
-import config.RedshiftSecondaryDependencies.{SecondaryMap, SecondaryBundle}
+import com.socrata.thirdparty.metrics.{Metrics, MetricsOptions, MetricsReporter}
+import config.RedshiftSecondaryDependencies.{SecondaryBundle, SecondaryMap}
+import io.agroal.api.AgroalDataSource
+import io.quarkus.agroal.DataSource
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Produces
 import org.apache.curator.framework.CuratorFramework
+
+import java.io.OutputStream
+import java.sql.Connection
+import javax.sql.{DataSource => JavaDataSource}
 
 object RedshiftSecondaryDependencies{
   type SecondaryBundle = (DSInfo, MetricsReporter, CuratorFramework)
@@ -20,10 +27,36 @@ object RedshiftSecondaryDependencies{
 class RedshiftSecondaryDependencies {
 
   @Produces
-  def dsInfo():DSInfo = ???
+  def dsInfo
+  (
+    @DataSource("store")
+    storeDataSource: AgroalDataSource
+  ):DSInfo = {
+    new DSInfo {
+      override val dataSource: JavaDataSource = storeDataSource
+      //TODO ??
+      override val copyIn: (Connection, String, OutputStream => Unit) => Long = ???
+    }
+  }
+
 
   @Produces
-  def metricsReporter(): MetricsReporter = ???
+  def metricsOptions(): MetricsOptions = {
+    MetricsOptions()
+  }
+
+  @Produces
+  def metricsRegistry(): MetricRegistry = {
+    Metrics.metricsRegistry
+  }
+  @Produces
+  def metricsReporter
+  (
+    metricsOptions: MetricsOptions,
+    metricRegistry: MetricRegistry
+  ): MetricsReporter = {
+    new MetricsReporter(metricsOptions,metricRegistry)
+  }
 
   @Produces
   def curator(): CuratorFramework = ???
