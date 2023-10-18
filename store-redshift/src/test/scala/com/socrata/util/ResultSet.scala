a9224cdad0cd38971dd060773cbee6f03a300645
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-18 12:26:14 -0400

    buffered iterator instead of stream

--- store-redshift/src/test/scala/com/socrata/util/ResultSet.scala
+++ store-redshift/src/test/scala/com/socrata/util/ResultSet.scala
@@ -4,12 +4,12 @@ import java.sql.ResultSet
 
 object ResultSet {
 
-  def extract[T](res: ResultSet)(f: ResultSet => T): Stream[T] = {
+  def extract[T](res: ResultSet)(f: ResultSet => T): BufferedIterator[T] = {
     new Iterator[T] {
       def hasNext = res.next()
 
       def next() = f(res)
-    }.toStream
+    }.buffered
   }
 
 }
