82e5ddbf49e41c8da160023a3f234f1f91f8c8d4
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-23 13:30:36 -0400

    checkpoint

--- store-redshift/src/main/scala/config/RedshiftSecondaryConfig.java
+++ store-redshift/src/main/scala/config/RedshiftSecondaryConfig.java
@@ -5,8 +5,6 @@ import com.socrata.datacoordinator.secondary.SecondaryWatcherAppConfig;
 import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 
-@StaticInitSafe
-@ConfigMapping(prefix = "redshift")
 public interface RedshiftSecondaryConfig extends SecondaryWatcherAppConfig {
 
 }
--- store-redshift/src/main/scala/config/RedshiftSecondaryDependencies.scala
+++ store-redshift/src/main/scala/config/RedshiftSecondaryDependencies.scala
@@ -1,9 +1,11 @@
 package config
 
 import com.codahale.metrics.MetricRegistry
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.socrata.datacoordinator.common.DataSourceFromConfig.DSInfo
 import com.socrata.datacoordinator.secondary.Secondary
 import com.socrata.datacoordinator.secondary.SecondaryWatcherApp.NumWorkers
+import com.socrata.datacoordinator.secondary.messaging.eurybates.MessageProducerConfig
 import com.socrata.soql.types.{SoQLType, SoQLValue}
 import com.socrata.thirdparty.metrics.{Metrics, MetricsOptions, MetricsReporter}
 import config.RedshiftSecondaryDependencies.{SecondaryBundle, SecondaryMap}
@@ -11,15 +13,19 @@ import io.agroal.api.AgroalDataSource
 import io.quarkus.agroal.DataSource
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Produces
+import jakarta.json.JsonObject
 import org.apache.curator.framework.{CuratorFramework, CuratorFrameworkFactory}
 import org.apache.curator.retry.BoundedExponentialBackoffRetry
+import org.eclipse.microprofile.config.ConfigProvider
 import service.RedshiftSecondary
 
-import java.io.OutputStream
+import java.io.{File, OutputStream}
 import java.sql.Connection
+import java.util.{Properties, UUID}
 import javax.sql.{DataSource => JavaDataSource}
+import scala.concurrent.duration.FiniteDuration
 
-object RedshiftSecondaryDependencies{
+object RedshiftSecondaryDependencies {
   type SecondaryBundle = (DSInfo, MetricsReporter, CuratorFramework)
   type SecondaryWithWorkers = (Secondary[SoQLType, SoQLValue], NumWorkers)
   type SecondaryMap = Map[String, SecondaryWithWorkers]
@@ -28,24 +34,30 @@ object RedshiftSecondaryDependencies{
 @ApplicationScoped
 class RedshiftSecondaryDependencies {
 
-object RedshiftCopyIn extends ((Connection, String, OutputStream => Unit) => Long) {
-  def apply(conn: Connection, sql: String, output: OutputStream => Unit): Long =
-    ???
-}
+  @Produces
+  def redshiftSecondaryConfig
+  (
+  objectMapper:ObjectMapper
+  ): RedshiftSecondaryConfig = {
+    //Seems to be tricky to implement scala -> java config types via "static" config. This is because of spi generic parameter limitations, type erasure and no arg constructor requirements.
+    //Lets simply read it as json. At this point we are past the "static" config bootstrap.
+    val asJson = ConfigProvider.getConfig().getValue("redshift",classOf[JsonObject])
+    //And use our configured objectMapper to marshall it. Our objectMapper knows how to convert scala -> java.
+    objectMapper.convertValue(asJson,classOf[RedshiftSecondaryConfig])
+  }
 
   @Produces
   def dsInfo
   (
     @DataSource("store")
     storeDataSource: AgroalDataSource
-  ):DSInfo = {
+  ): DSInfo = {
     new DSInfo {
       override val dataSource: JavaDataSource = storeDataSource
       //TODO ??
       override val copyIn = RedshiftCopyIn
     }
   }
-
 
   @Produces
   def metricsOptions(): MetricsOptions = {
@@ -56,13 +68,14 @@ object RedshiftCopyIn extends ((Connection, String, OutputStream => Unit) => Lon
   def metricsRegistry(): MetricRegistry = {
     Metrics.metricsRegistry
   }
+
   @Produces
   def metricsReporter
   (
     metricsOptions: MetricsOptions,
     metricRegistry: MetricRegistry
   ): MetricsReporter = {
-    new MetricsReporter(metricsOptions,metricRegistry)
+    new MetricsReporter(metricsOptions, metricRegistry)
   }
 
   @Produces
@@ -85,9 +98,15 @@ object RedshiftCopyIn extends ((Connection, String, OutputStream => Unit) => Lon
     curatorFramework: CuratorFramework
   ): SecondaryBundle = (dsInfo, metricsReporter, curatorFramework)
 
+  @Produces
+  def secondaries(secondary: RedshiftSecondary): SecondaryMap = Map("redshift" -> (secondary, 1))
+
   //TODO: Map keys will need to be dynamic
 
-  @Produces
-  def secondaries(secondary: RedshiftSecondary): SecondaryMap = Map("redshift" -> (secondary,1))
+  object RedshiftCopyIn extends ((Connection, String, OutputStream => Unit) => Long) {
+    def apply(conn: Connection, sql: String, output: OutputStream => Unit): Long =
+      ???
+  }
+
 
 }
