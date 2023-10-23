ffbfbc208497a655a698f31a57105cdf065c8377
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-10-23 17:13:21 -0400

    autoformat

--- store-redshift/src/test/scala/com/socrata/util/ResultSet.scala
+++ store-redshift/src/test/scala/com/socrata/util/ResultSet.scala
@@ -4,20 +4,20 @@ import java.sql.ResultSet
 
 object ResultSet {
 
+  def extractHeadOption[T](resultSet: ResultSet)(mappingFunction: ResultSet => T): Option[T] = {
+    extractThen(resultSet)(mappingFunction)(_.headOption)
+  }
+
+  def extractThen[T, M](resultSet: ResultSet)(mappingFunction: ResultSet => T)(collectingFunction: BufferedIterator[T] => M): M = {
+    collectingFunction(extract(resultSet)(mappingFunction))
+  }
+
   def extract[T](resultSet: ResultSet)(mappingFunction: ResultSet => T): BufferedIterator[T] = {
     new Iterator[T] {
       def hasNext = resultSet.next()
 
       def next() = mappingFunction(resultSet)
     }.buffered
-  }
-
-  def extractThen[T,M](resultSet: ResultSet)(mappingFunction: ResultSet => T)(collectingFunction: BufferedIterator[T] => M): M={
-    collectingFunction(extract(resultSet)(mappingFunction))
-  }
-
-  def extractHeadOption[T](resultSet: ResultSet)(mappingFunction: ResultSet => T): Option[T] = {
-    extractThen(resultSet)(mappingFunction)(_.headOption)
   }
 
 }
