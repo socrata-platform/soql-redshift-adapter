12b0375e339dd9694736bf4262ff6d6b8cdfbf7c
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-11-20 16:06:45 -0500

    more tests

--- common-redshift/src/test/scala/com/socrata/config/JacksonProxyConfigTest.scala
+++ common-redshift/src/test/scala/com/socrata/config/JacksonProxyConfigTest.scala
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.{DisplayName, Test}
 @DisplayName("Jackson Proxy Config Tests")
 class JacksonProxyConfigTest {
 
-  @DisplayName("Person sanity test")
+  @DisplayName("Nested traits")
   @Test
   def person():Unit={
 
@@ -32,6 +32,49 @@ class JacksonProxyConfigTest {
     assert("United States".equals(person.address().country()))
     assert("New York".equals(person.address().state()))
     assert("Buffalo".equals(person.address().city()))
+  }
+
+  @DisplayName("List")
+  @Test
+  def list(): Unit = {
+
+    trait Recipe {
+      def ingredients(): List[String]
+    }
+
+    val recipe: Recipe =
+      JacksonProxyConfigBuilder(CommonObjectMapperCustomizer.Default)
+        .withSources(JacksonYamlConfigSource("data/recipe.yaml"))
+        .proxy( classOf[Recipe])
+
+    assert(List("Tomato","Cheese","Bread").equals(recipe.ingredients()))
+  }
+
+  @DisplayName("List of complex type")
+  @Test
+  def listComplex(): Unit = {
+
+    trait Ingredient{
+      def name(): String
+      def amount(): Int
+    }
+
+    trait Recipe {
+      def ingredients(): List[Ingredient]
+    }
+
+    val recipe: Recipe =
+      JacksonProxyConfigBuilder(CommonObjectMapperCustomizer.Default)
+        .withSources(JacksonYamlConfigSource("data/recipe2.yaml"))
+        .proxy(classOf[Recipe])
+
+    assert(
+      List(
+        Map("name" -> "Tomato", "amount" -> 3),
+        Map("name" -> "Cheese", "amount" -> 32),
+        Map("name" -> "Bread", "amount" -> 1)
+      ).equals(recipe.ingredients())
+    )
   }
 
 }
