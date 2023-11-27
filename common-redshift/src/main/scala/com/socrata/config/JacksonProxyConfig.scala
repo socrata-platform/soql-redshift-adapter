c00495025326d9abc3cf24b32b51e8899f74d6c4
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-11-27 10:53:45 -0500

    some cleanup

--- common-redshift/src/main/scala/com/socrata/config/JacksonProxyConfig.scala
+++ common-redshift/src/main/scala/com/socrata/config/JacksonProxyConfig.scala
@@ -6,6 +6,7 @@ import com.socrata.config.JacksonProxyConfigBuilder.merge
 
 import java.lang.reflect.{InvocationHandler, Method, ParameterizedType, Proxy}
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.socrata.config.JsonNodeBackedJacksonInvocationHandler.{innerGenericClass, kebab}
 
 import scala.util.{Failure, Success, Try}
 import scala.collection.JavaConverters._
@@ -40,6 +41,7 @@ object JacksonProxyConfigBuilder{
 }
 
 trait ConfigSource {
+  //Anything that can produce json
   def read(): JsonNode
 }
 
@@ -48,7 +50,7 @@ trait ConfigProvider{
   def proxy[T](path: String, clazz: Class[T]): T
   //Use the root to get the config interface/trait.
   def proxy[T](clazz: Class[T]): T
-  //Once you chunk, you lose access to ConfigBuilder and cannot source further.
+  //Selects a subset of configs based on prefix, Once you chunk, you lose access to ConfigBuilder and cannot source further.
   def chunk(path:String): ConfigProvider
 }
 
@@ -66,52 +68,83 @@ case class JacksonProxyConfigBuilder(private val objectMapper:ObjectMapper) exte
     )
 }
 
-//Uses Jacksons to delegate method calls of a proxy, backed by a JsonNode. Uses an ObjectMapper to convert/marshall stuff.
+object JsonNodeBackedJacksonInvocationHandler{
+  def innerGenericClass(method: Method): Class[_] = {
+    Class.forName(method.getGenericReturnType.asInstanceOf[ParameterizedType].getActualTypeArguments.head.getTypeName)
+  }
+
+  //We will strictly use kebab case
+  def kebab(in: String): String = {
+    PropertyNamingStrategies.KebabCaseStrategy.INSTANCE.translate(in)
+  }
+}
+
+//Uses Jackson to delegate method calls of a proxy, backed by a JsonNode. Uses an ObjectMapper to convert/marshall stuff.
 case class JsonNodeBackedJacksonInvocationHandler(data: JsonNode, objectMapper:ObjectMapper) extends InvocationHandler {
   def invoke(proxy: scala.AnyRef, method: Method, args: Array[AnyRef]): AnyRef = {
-    method.getName match{
+    val out = method.getName match{
       //Simple implementation for toString, when the root interface is accessed directly.
-      case "toString" => data.toString //Could also make it pretty?
+      case "toString" => data.toString
       //implement others when it becomes important...ie equals/hash...not sure this matters much for our configs yet?
-      case methodName => data.findValue(PropertyNamingStrategies.KebabCaseStrategy.INSTANCE.translate(methodName)) match {
+
+
+      case methodName => data.findValue(kebab(methodName)) match {
         case null => throw new NotImplementedError(s"Unsupported method name '$methodName', implement it above (probably)!")
-        //Its an object, so instead of using reflection - lets also construct a proxy for this child. This lets us use nested interfaces, neat.
-        case value: ObjectNode => method.getReturnType.getName match {
-          //TODO Option of simple vs Option of complex, later check if target class is interface or not, rather than try.
-          case "scala.Option" => Try(JacksonProxyConfigProvider(value, objectMapper).proxy(Class.forName(method.getGenericReturnType.asInstanceOf[ParameterizedType].getActualTypeArguments.head.getTypeName)).asInstanceOf[AnyRef]) match {
-            case Failure(exception) => Try(objectMapper.readValue(
-              value.toString,
-              Class.forName(method.getGenericReturnType.asInstanceOf[ParameterizedType].getActualTypeArguments.head.getTypeName)
-            ).asInstanceOf[AnyRef]) match {
-              case Failure(exception) => None
-              case Success(value) => Some(value)
-            }
-            case Success(value) => Some(value)
-          }
-          case _ => JacksonProxyConfigProvider(value, objectMapper).proxy(method.getReturnType).asInstanceOf[AnyRef]
+        case value: ObjectNode => handleObject(value,method)
+        case value: ArrayNode => handleArray(value.elements().asScala,innerGenericClass(method))
+        case value => doConvert(value.toString, method.getReturnType)
+      }
+    }
+    //Any -> AnyRef
+    out.asInstanceOf[AnyRef]
+  }
+
+  private def handleObject[T](data: JsonNode, method: Method): Any = {
+    method.getReturnType.getName match {
+      //TODO Option of simple vs Option of complex, later check if target class is interface or not, rather than try.
+      case "scala.Option" => Try(doProxy(data, innerGenericClass(method))) match {
+        case Failure(exception) => Try(doConvert(
+          data.toString,
+          innerGenericClass(method)
+        )) match {
+          case Failure(_) => None
+          case Success(value) => Some(value)
         }
-        case value: ArrayNode => value.elements().asScala.toList.map {
-          //TODO Array item of simple vs Array item of complex, same here, later check if target class is interface or not, rather than try
-          case item: ObjectNode => Try(JacksonProxyConfigProvider(item, objectMapper).proxy(Class.forName(method.getGenericReturnType.asInstanceOf[ParameterizedType].getActualTypeArguments.head.getTypeName)).asInstanceOf[AnyRef]) match {
-            case Failure(_) => objectMapper.readValue(
-              item.toString,
-              Class.forName(method.getGenericReturnType.asInstanceOf[ParameterizedType].getActualTypeArguments.head.getTypeName)
-            ).asInstanceOf[AnyRef]
-            case Success(value) =>  value
-          }
-          case item => objectMapper.readValue(
-            item.toString,
-            Class.forName(method.getGenericReturnType.asInstanceOf[ParameterizedType].getActualTypeArguments.head.getTypeName)
-          ).asInstanceOf[AnyRef]
-        }
-        //Convert the JsonNode object to something typed, like a TextNode to a String...etc
-        case value => objectMapper.readValue(
-          value.toString,
-          method.getReturnType
-        ).asInstanceOf[AnyRef]
+        case Success(value) => Some(value)
+      }
+      case _ => Try(doProxy(data, method.getReturnType)) match {
+        case Failure(_) => doConvert(data.toString, method.getReturnType)
+        case Success(value) => value
       }
     }
   }
+  private def handleArray[T](data:Iterator[JsonNode],target:Class[T]):List[T]={
+    data.toList.map {
+      //TODO Array item of simple vs Array item of complex, same here, later check if target class is interface or not, rather than try
+      case item: ObjectNode => Try(doProxy(item, target)) match {
+        case Failure(_) => doConvert(
+          item.toString,
+          target
+        )
+        case Success(value) => value
+      }
+      case item => doConvert(
+        item.toString,
+        target
+      )
+    }
+  }
+
+  //Convert the JsonNode to something else
+  private def doConvert[T](value:String, returning: Class[T]):T={
+    objectMapper.readValue(value,returning)
+  }
+
+  //Proxy the JsonNode as an interface
+  private def doProxy[T](data: JsonNode, target: Class[T]): T = {
+    JacksonProxyConfigProvider(data, objectMapper).proxy(target)
+  }
+
 }
 
 case class JacksonProxyConfigProvider(private val data: JsonNode, private val objectMapper:ObjectMapper) extends ConfigProvider with ConfigBuilder {
