6cfa816b4fa177ff690c3331d5946e068e672d7b
zlef <zachlefevre@gmail.com>
2024-01-02 12:53:27 -0600

    dummy impl

--- store-redshift/src/main/scala/com/socrata/db/datasets/TableCreator.scala
+++ store-redshift/src/main/scala/com/socrata/db/datasets/TableCreator.scala
@@ -6,3 +6,9 @@ import com.socrata.db.Exists
 trait TableCreator {
   def create(dataset: Dataset, blobUrl: String): Exists.Exists[String]
 }
+
+@jakarta.enterprise.context.ApplicationScoped
+object TableCreator extends TableCreator {
+
+  override def create(dataset: Dataset, blobUrl: String): Exists.Exists[String] = ???
+}
