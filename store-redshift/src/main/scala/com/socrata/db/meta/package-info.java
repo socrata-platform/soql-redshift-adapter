f248795717a2e43cc0515db5c404f00603adddcc
zlef <zachlefevre@gmail.com>
2024-01-08 15:17:15 -0600

    delete dead packages

--- store-redshift/src/main/scala/com/socrata/db/meta/package-info.java
+++
@@ -1,6 +0,0 @@
-@PersistenceUnit("db/meta")
-package com.socrata.db.meta;
-
-import io.quarkus.hibernate.orm.PersistenceUnit;
-
-// https://quarkus.io/guides/hibernate-orm#multiple-persistence-units
--- store-redshift/src/main/scala/com/socrata/db/store/package-info.java
+++
@@ -1,6 +0,0 @@
-@PersistenceUnit("store")
-package com.socrata.db.store;
-
-import io.quarkus.hibernate.orm.PersistenceUnit;
-
-// https://quarkus.io/guides/hibernate-orm#multiple-persistence-units
